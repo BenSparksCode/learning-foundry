2b8eee3bec133b21fbb1436d1af61e2ab186e237
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-01-17 20:17:29 +0200

    Testing positives and fail cases

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.10;
 
-import "ds-test/test.sol";
+import 'ds-test/test.sol';
 
 contract ContractTest is DSTest {
-    function setUp() public {}
+	uint256 a;
 
-    function testExample() public {
-        assertTrue(true);
-    }
+	function setUp() public {
+		a = 10;
+	}
+
+	// To test a successful case - prefix test
+	function testExample() public {
+		assertEq(a, 10);
+	}
+
+	// To test a failure case - prefix testFail
+	function testFailExample() public {
+		assertEq(a, 9);
+	}
 }
